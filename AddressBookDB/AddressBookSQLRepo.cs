d34948f21ffbe3a4d1e00b7f84ffd3104c2bda4a
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-26 17:23:00 +0530

    [soham]Added:Exception handling for GetDetaisl

--- AddressBookDB/AddressBookSQLRepo.cs
+++ AddressBookDB/AddressBookSQLRepo.cs
@@ -11,36 +11,43 @@ namespace AddressBookDB
         readonly SqlConnection connection = new SqlConnection(ConnectionString);
         public void GetDetails()
         {
-            AddressBookModel addressBookModel = new AddressBookModel();
-            using (this.connection)
+            try
             {
-                string query = @"SELECT * FROM AddressBook ";
-                SqlCommand cmd = new SqlCommand(query, connection);
-                connection.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if(reader.HasRows)
+                AddressBookModel addressBookModel = new AddressBookModel();
+                using (this.connection)
                 {
-                    while(reader.Read())
+                    string query = @"SELECT * FROM AddressBook ";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
                     {
-                        addressBookModel.ID = reader.GetInt32(0);
-                        addressBookModel.FirstName = reader.GetString(1);
-                        addressBookModel.LastName = reader.GetString(2);
-                        addressBookModel.Address = reader.GetString(3);
-                        addressBookModel.City = reader.GetString(4);
-                        addressBookModel.State = reader.GetString(5);
-                        addressBookModel.Zip = reader.GetString(6);
-                        addressBookModel.PhoneNumber = reader.GetString(7);
-                        addressBookModel.Email = reader.GetString(8);
-                        addressBookModel.Type = reader.GetString(9);
-                        addressBookModel.AddressBookName = reader.GetString(11);
-                        Console.WriteLine(addressBookModel.ID+ " " +addressBookModel.FirstName + " " + addressBookModel.LastName + " " + addressBookModel.Address + " " + addressBookModel.City + " " + addressBookModel.State + " " + addressBookModel.Zip + " " + addressBookModel.PhoneNumber + " " + addressBookModel.Email + " " + addressBookModel.Type + " " + addressBookModel.AddressBookName);
-                        Console.WriteLine("\n");
+                        while (reader.Read())
+                        {
+                            addressBookModel.ID = reader.GetInt32(0);
+                            addressBookModel.FirstName = reader.GetString(1);
+                            addressBookModel.LastName = reader.GetString(2);
+                            addressBookModel.Address = reader.GetString(3);
+                            addressBookModel.City = reader.GetString(4);
+                            addressBookModel.State = reader.GetString(5);
+                            addressBookModel.Zip = reader.GetString(6);
+                            addressBookModel.PhoneNumber = reader.GetString(7);
+                            addressBookModel.Email = reader.GetString(8);
+                            addressBookModel.Type = reader.GetString(9);
+                            addressBookModel.AddressBookName = reader.GetString(11);
+                            Console.WriteLine(addressBookModel.ID + " " + addressBookModel.FirstName + " " + addressBookModel.LastName + " " + addressBookModel.Address + " " + addressBookModel.City + " " + addressBookModel.State + " " + addressBookModel.Zip + " " + addressBookModel.PhoneNumber + " " + addressBookModel.Email + " " + addressBookModel.Type + " " + addressBookModel.AddressBookName);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Found");
                     }
                 }
-                else
-                {
-                    Console.WriteLine("No Data Found");
-                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
